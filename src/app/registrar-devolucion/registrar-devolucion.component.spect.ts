564084ef401268840cca558c98e130c8571a85aa
GabrielHV9 <gdhernandezv@correo.udistrital.edu.co>
2023-05-31 16:06:25 -0500

    interfaz devolucion
    interfaz devolucion

---
+++ src/app/registrar-devolucion/registrar-devolucion.component.spect.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrarDevolucionComponent } from './registrar-devolucion.component';
+
+describe('RegistrarDevolucionComponent', () => {
+  let component: RegistrarDevolucionComponent;
+  let fixture: ComponentFixture<RegistrarDevolucionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegistrarDevolucionComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegistrarDevolucionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/registrar-devolucion/registrar-devolucion.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductoService} from 'src/app/services/producto.service';
+import {InventarioService} from 'src/app/services/inventario.service'
+import { invProdData } from '../types/invProd';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-registrar-Devolucion',
+  templateUrl: './registrar-Devolucion.component.html',
+  styleUrls: ['./registrar-Devolucion.component.css']
+})
+export class RegistrarDevolucionComponent implements OnInit {
+
+  listaProductos:any
+  invProdForm:FormGroup
+
+  constructor(
+    private productoService: ProductoService,
+    private inventarioService: InventarioService,
+    private formBuilder:FormBuilder){
+      this.invProdForm = this.formBuilder.group({
+        cantidad_inv:0,
+        cantidad_vend:0,
+        updated:"",
+        inventarioIdinventario:0,
+        productoIdproducto:0
+      });
+
+    }
+    
+  ngOnInit(): void {
+    this.productoService.list().subscribe((response)=>{
+      this.listaProductos=response
+    })
+  }
+
+  onSubmit():void{
+    const producto = {
+      cantidad_inv: this.invProdForm.value.cantidad_inv,
+      cantidad_vend: 0,
+      updated: new Date(),
+      inventarioIdinventario:5,
+      productoIdproducto:Number(this.invProdForm.value.productoIdproducto)
+    };
+   
+    this.inventarioService.createInvProd(producto).subscribe((response)=>{
+      console.log(response)
+  
+        if(response.status){
+          Swal.fire('Error', response.message, 'error');
+        }else{
+          Swal.fire('listo', 'Devolucion registrado satisfactoriamente', 'success');
+        }
+        
+      
+     
+    })
+  }
+
+}
